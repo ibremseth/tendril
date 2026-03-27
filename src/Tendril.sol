44044c47a80801b9ad0e1e3259baff2b8f12807e
Isaac <isaac@syndicate.io>
2026-03-27 12:04:21 -0400

    Initial draft of contract + some basic scripts

---
+++ src/Tendril.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract Seed is UUPSUpgradeable {
+    function _authorizeUpgrade(address) internal override {}
+}
+
+contract Tendril {
+    address public immutable ADMIN;
+    address public immutable SEED;
+
+    event NewDeployment(address deployAddress);
+
+    constructor(address root, uint256 rootChainId) {
+        if (block.chainid == rootChainId) {
+            ADMIN = root;
+        } else {
+            ADMIN = address(uint160(address(this)) + uint160(0x1111000000000000000000000000000000001111));
+        }
+        SEED = address(new Seed());
+    }
+
+    modifier onlyAdmin() {
+        _onlyAdmin();
+        _;
+    }
+
+    function _onlyAdmin() internal view {
+        require(msg.sender == ADMIN);
+    }
+
+    function execute(address dest, bytes calldata data) external payable onlyAdmin {
+        (bool success, bytes memory result) = payable(dest).call{value: msg.value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    function deploy(bytes32 salt, address impl, bytes calldata init) external onlyAdmin {
+        address deployAddress =
+            Create2.deploy(0, salt, abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(SEED, "")));
+        (bool success, bytes memory result) =
+            deployAddress.call(abi.encodeWithSignature("upgradeToAndCall(address,bytes)", impl, init));
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+        emit NewDeployment(deployAddress);
+    }
+
+    function predict(bytes32 salt) external view returns (address) {
+        return Create2.computeAddress(
+            salt, keccak256(abi.encodePacked(type(ERC1967Proxy).creationCode, abi.encode(SEED, "")))
+        );
+    }
+
+    receive() external payable {}
+}
